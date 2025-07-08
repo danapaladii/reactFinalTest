bb96fb9af3b3b1158a6f43703df6989be64239fc
danapaladii <154483015+danapaladii@users.noreply.github.com>
2025-07-08 19:15:23 -0400

    Update AllCategories.js

--- src/Filter/AllCategories.js
+++ src/Filter/AllCategories.js
@@ -4,7 +4,7 @@ import Filter from "./Filter";
 const AllCategories = () => {
     return(<div>
         <h2>Choose Your Favorite Season</h2>
-        {['Spring', 'Summer', 'Fall', 'All'].map(category =><Filter category={category}/>)}
+        {['Spring', 'Summer', 'Fall', 'All'].map(category =><Filter key={category} category={category}/>)}
         </div>)
 }
 export default AllCategories;
